002e982b49192ca1244cb8a63bf2184513c742c7
Rafael Sanguinetti <rafael.sanguinett@onebytedata.com>
2020-06-16 15:30:04 -0300

    Added C-Move service

--- dimsec/cmove.go
+++ dimsec/cmove.go
@@ -5,10 +5,10 @@ import (
 	"git.onebytedata.com/OneByteDataPlatform/go-dicom/network"
 )
 
-func CFindReadRQ(pdu network.PDUService, DCO media.DcmObj, DDO *media.DcmObj) bool {
+func CMoveReadRQ(pdu network.PDUService, DCO media.DcmObj, DDO *media.DcmObj) bool {
 	if DCO.TagCount() != 0 {
-		// Is this a C-Find?
-		if DCO.GetUShort(0x00, 0x100) == 0x20 {
+		// Is this a C-Move?
+		if DCO.GetUShort(0x00, 0x100) == 0x21 {
 			// Does it have data?
 			if DCO.GetUShort(0x00, 0x0800) != 0x0101 {
 				return pdu.Read(DDO)
@@ -18,23 +18,29 @@ func CFindReadRQ(pdu network.PDUService, DCO media.DcmObj, DDO *media.DcmObj) bo
 	return false
 }
 
-func CFindWriteRQ(pdu network.PDUService, DDO media.DcmObj, SOPClassUID string) bool {
+func CMoveWriteRQ(pdu network.PDUService, DDO media.DcmObj, SOPClassUID string, AETDest string) bool {
 	var DCO media.DcmObj
 	var size uint32
-	var valor uint16
+	var valor, largo uint16
+
+	largo = uint16(len(AETDest))
+	if largo%2 == 1 {
+		largo++
+	}
 
 	valor = uint16(len(SOPClassUID))
 	if valor%2 == 1 {
 		valor++
 	}
 
-	size = uint32(8 + valor + 8 + 2 + 8 + 2 + 8 + 2)
+	size = uint32(8 + valor + 8 + 2 + 8 + 2+ 8+ largo + 8 + 2+ 8+ 2)
 
 	DCO.WriteUint32(0x00, 0x00, "UL", size)                  // Length
 	DCO.WriteString(0x0000, 0x0002, "UI", SOPClassUID)       //SOP Class UID
-	DCO.WriteUint16(0x00, 0x0100, "US", 0x20)                //Command Field
+	DCO.WriteUint16(0x00, 0x0100, "US", 0x21)                //Command Field
 	DCO.WriteUint16(0x00, 0x0110, "US", network.Uniq16odd()) //Message ID
-	DCO.WriteUint16(0x00, 0x0700, "US", 0x00)                //Data Set type
+	DCO.WriteString(0x00, 0x0600, "AE", AETDest)             // Destination AET
+	DCO.WriteUint16(0x00, 0x0700, "US", 0x00)                // Priority
 	DCO.WriteUint16(0x00, 0x0800, "US", 0x0102)              //Data Set type
 
 	if pdu.Write(DCO, SOPClassUID, 0x01) {
@@ -43,7 +49,7 @@ func CFindWriteRQ(pdu network.PDUService, DDO media.DcmObj, SOPClassUID string) 
 	return false
 }
 
-func CFindReadRSP(pdu network.PDUService, DDO *media.DcmObj) int {
+func CMoveReadRSP(pdu network.PDUService, DDO *media.DcmObj, pending *int) int {
 	var DCO media.DcmObj
 	status := -1
 
@@ -51,53 +57,48 @@ func CFindReadRSP(pdu network.PDUService, DDO *media.DcmObj) int {
 		return status
 	}
 	// Is this a C-Find RSP?
-	if DCO.GetUShort(0x00, 0x0100) == 0x8020 {
+	if DCO.GetUShort(0x00, 0x0100) == 0x8021 {
 		if DCO.GetUShort(0x00, 0x0800) != 0x0101 {
 			if pdu.Read(DDO) {
 				status = int(DCO.GetUShort(0x00, 0x0900)) // Return Status
+				*pending = int(DCO.GetUShort(0x00, 0x1020))
 			} else {
 				status=-1
 			}
 		} else {
 			status = int(DCO.GetUShort(0x00, 0x0900)) // Return Status			
+			*pending = -1
 		}
 	}
 	return status
 }
 
-func CFindWriteRSP(pdu network.PDUService, DCO media.DcmObj, DDO media.DcmObj, status uint16) bool {
+func CMoveWriteRSP(pdu network.PDUService, DCO media.DcmObj, status uint16, pending uint16) bool {
 	var DCOR media.DcmObj
 	var size uint32
-	var sopclasslength, leDSType uint16
 	flag := false
 
 	DCOR.TransferSyntax = DCO.TransferSyntax
 
-	if DDO.TagCount() > 0 {
-		leDSType = 0x0102
-	} else {
-		leDSType = 0x0101
-	}
 	SOPClassUID := DCO.GetString(0x00, 0x02)
-	sopclasslength = uint16(len(SOPClassUID))
+	sopclasslength := uint16(len(SOPClassUID))
 	if sopclasslength > 0 {
 		if sopclasslength%2 == 1 {
 			sopclasslength++
 		}
 
-		size = uint32(8 + sopclasslength + 8 + 2 + 8 + 2 + 8 + 2)
+		size = uint32(8 + sopclasslength + 8 + 2 + 8 + 2 + 8 + 2+8+2+8+2)
 
 		DCOR.WriteUint32(0x00, 0x00, "UL", size)        // Length
 		DCOR.WriteString(0x00, 0x02, "UI", SOPClassUID) //SOP Class UID
-		DCOR.WriteUint16(0x00, 0x0100, "US", 0x8020)    //Command Field
+		DCOR.WriteUint16(0x00, 0x0100, "US", 0x8021)    //Command Field
 		valor := DCO.GetUShort(0x00, 0x0110)
 		DCOR.WriteUint16(0x00, 0x0120, "US", valor)    //Message ID
-		DCOR.WriteUint16(0x00, 0x0800, "US", leDSType) //Data Set type
-		DCOR.WriteUint16(0x00, 0x0900, "US", status)   //Data Set type
+		DCOR.WriteUint16(0x00, 0x0800, "US", 0x101)  //Data Set type
+		DCOR.WriteUint16(0x00, 0x0900, "US", status)   //Status
+		DCOR.WriteUint16(0x00, 0x1020, "US", pending)   //Pending
+
 		flag = pdu.Write(DCOR, SOPClassUID, 0x01)
-		if flag && (DDO.TagCount() > 0) {
-			flag = pdu.Write(DDO, SOPClassUID, 0x00)
-		}
 	}
 	return flag
 }
--- test/client.go
+++ test/client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"git.onebytedata.com/OneByteDataPlatform/go-dicom/media"
 	"git.onebytedata.com/OneByteDataPlatform/go-dicom/services"
 )
@@ -10,10 +11,33 @@ func client(AET string, IP string, Port string) {
 	media.InitDict()
 	if services.EchoSCU("TESTSCU", AET, IP, Port, 30) {
 		fmt.Println("DICOM Echo OK!!")
-		if services.StoreSCU( "TESTSCU", AET, IP, Port, "test.dcm", 30){
+		if services.StoreSCU("TESTSCU", AET, IP, Port, "test.dcm", 30) {
 			fmt.Println("DICOM Store OK!!")
 		} else {
 			fmt.Println("DICOM Store Failed!!")
+		}
+		var Results []media.DcmObj
+		var Query media.DcmObj
+		Query.WriteString(0x08, 0x20, "DA", "")
+		Query.WriteString(0x08, 0x30, "TM", "")
+		Query.WriteString(0x08, 0x50, "SH", "")
+		Query.WriteString(0x08, 0x52, "CS", "STUDY") // Use Study Level
+		Query.WriteString(0x08, 0x61, "CS", "MR")    // Look for Modality=MR
+		Query.WriteString(0x08, 0x1030, "LO", "")
+		Query.WriteString(0x10, 0x10, "PN", "")
+		Query.WriteString(0x10, 0x20, "LO", "")
+		Query.WriteString(0x10, 0x30, "DA", "")
+		Query.WriteString(0x10, 0x40, "CS", "")
+		Query.WriteString(0x20, 0x0D, "UI", "")
+		if services.FindSCU("TESTSCU", AET, IP, Port, Query, &Results, 30) == 0 {
+			fmt.Println("DICOM Query OK!! Results: " + strconv.Itoa(len(Results)))
+		} else {
+			fmt.Println("DICOM Query Failed!!")
+		}
+		if services.MoveSCU("TESTSCU", AET, IP, Port, "DESTAET", Query, 30) == 0 {
+			fmt.Println("DICOM Move OK!!")
+		} else {
+			fmt.Println("DICOM Move Failed!!")
 		}
 	} else {
 		fmt.Println("DICOM Echo Failed!!")

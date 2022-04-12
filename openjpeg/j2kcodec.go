3276baa13353f4cd0b718ff47f3d650413f3d1a0
DeYung Le <deyung@gmail.com>
2022-04-12 17:56:35 -0500

    Added openjpeg libs

--- openjpeg/j2kcodec.go
+++ openjpeg/j2kcodec.go
@@ -1,7 +1,7 @@
 package openjpeg
 
-// #cgo CFLAGS: -I j2klib/include -I j2klib/linux
-// #cgo LDFLAGS: -L j2klib/linux -llibopenjpeg.so.1.5.2
+// #cgo CFLAGS: -I j2klib/include -I j2klib/macos
+// #cgo LDFLAGS: -L j2klib/macos -lopenjpeg
 // #include "j2klib/decomj2k.c"
 // #include "j2klib/comj2k.c"
 import  "C"

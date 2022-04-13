7cee2159fea7d2b534a0b8700b9fe6dff02d2797
DeYung Le <deyung@gmail.com>
2022-04-12 23:07:55 -0500

    updated openjpeg lib

---
+++ changeLinux-amd64.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+sed -i 's/darwin/linux/g' openjpeg/j2kcodec.go
+sed -i 's/darwin/linux/g' jpeglib/codec8.go
+sed -i 's/darwin/linux/g' jpeglib/codec12.go
+sed -i 's/darwin/linux/g' jpeglib/codec16.go
+
+rm -rf jpeglib/dcmjpeg/linux
+cp -a jpeglib/dcmjpeg/linux-arm64 jpeglib/dcmjpeg/linux
+
+rm -rf openjpeg/j2klib/linux
+cp -a openjpeg/j2klib/linux-arm64 openjpeg/j2klib/linux
